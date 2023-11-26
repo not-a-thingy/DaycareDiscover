695c8062ec1751469e3fe9bed76ed786fbd65e33
LeeZheeNan <zheenan10@gmail.com>
2023-11-26 14:32:25 +0800

    move web.php file back to routes folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bookvisit', [AuthManager::class,'bookvisit'])->name('bookvisit');
+Route::post('/bookvisit', [AuthManager::class,'bookvisitPost'])->name('bookvisit.post');
+
+
