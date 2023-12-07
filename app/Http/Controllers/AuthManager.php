bdce927c0281c7b8ae697d59fdd2f2caf6221f83
not-a-thingy <76759140+not-a-thingy@users.noreply.github.com>
2023-12-07 10:03:45 +0800

    separated addvisit to another file

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Addvisit;
 use App\Models\Testvisit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -29,27 +28,6 @@ class AuthManager extends Controller
         }
         return redirect(route('bookvisit'))->with('success','Please wait for approval'); 
 }
-
-    function addvisit(){
-        return view('addvisit');
-    }
-
-    function addvisitPost(Request $request){
-
-        $request->validate([
-             'date' => 'required',
-             'time' => 'required'
-         ]);
- 
-         $data['date'] = $request->date;
-         $data['time'] = $request->time;
-         $addvisit = Addvisit::create($data);
-         if(!$addvisit){
-             return redirect(route('addvisit'))->with('error','Please try again');
-         }
-         return redirect(route('addvisit'))->with('Success'); 
- }
-
 
         /*function bookcheckPost(Request $request){
             $credentials = $request->only('date','time');
