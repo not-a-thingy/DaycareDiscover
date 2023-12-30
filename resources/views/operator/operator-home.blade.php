5f100dd4615b988b65f769e7cf843fd23b7f363f
afiq <59683477+ayiqazmi@users.noreply.github.com>
2023-12-30 21:26:10 +0800

    -

--- resources/views/operator/operator-home.blade.php
+++ resources/views/operator/operator-home.blade.php
@@ -839,65 +839,7 @@ legend {
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      
-    </ul>
-
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a style="color:black;" class="nav-link" href="#">DayCare<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item dropdown dmenu">
-            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-            {{ Auth::user()->name }}
-            </a>
-            <div class="dropdown-menu sm-menu">
-              <a class="dropdown-item" href="{{ url('/users') }}">Profile</a>
-                     
-              <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                
-
-                                    
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-            
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Call</a>
-          </li>
-       
-          </ul>
-          <div class="social-part">
-            <i class="fa fa-facebook" aria-hidden="true"></i>
-            <i class="fa fa-twitter" aria-hidden="true"></i>
-            <i class="fa fa-instagram" aria-hidden="true"></i>
-          </div>
-        </div>
-    
-  </nav>
- 
+  
   <div class="s01">
     <img src="{{ asset('img/24488829_xxl.jpg') }}" style="width:100%; height:100% z-index:-1;" alt="">
    
@@ -921,6 +863,8 @@ legend {
     </div>
    
     <br><br>
+
+    
 
     
  
