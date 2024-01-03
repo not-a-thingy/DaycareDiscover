305ecbbcfd803e5ad468de5fac80d7910927ecc1
afiq <59683477+ayiqazmi@users.noreply.github.com>
2024-01-03 23:06:06 +0800

    Update show_daycare.blade.php

--- resources/views/include/show_daycare.blade.php
+++ resources/views/include/show_daycare.blade.php
@@ -13,7 +13,7 @@
 
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper" style="margin:0; height: 100px; width:100%;">
+  <div class="content-wrapper" style="margin: 0; height: 100px; background-image: url('/img/bg.jpg'); background-size: 900px; background-repeat: repeat;">
   <!-- Content Header (Page header) -->
   <div class="content-header">
     <div class="container">
